491e9a532fdece8dcbb48bb69af33e7059a1e1d1
Gustavo <versace123mx@gmail.com>
2024-08-05 03:09:21 -0600

    se agrego typescript para jquery para que pueda interpretarlo correctamente, del app.componente.html se quito la clase de activo ya que siempre aparecia como activo, y en el timeline.component.ts se creo la funcionalidad para llamar a las publicaciones se maquetaron y se realizo la logica para que el paginado sea infinito, se llama a la funcion de traer los registros del api y cuando se da click en ver mas este llama a la pagina dos, pero el resultado de la pagina 1 se queda guardado en el array, cuando llamas a pagina dos, ya tiene los datos anteriores mas los nuevos y esto genera la ilucion de un scroll infinito, pero en realizad cada peticin es independentiende, solo que se van guardando los resultados en un acumulador

--- src/app/components/timeline/timeline.component.ts
+++ src/app/components/timeline/timeline.component.ts
@@ -6,32 +6,35 @@ import { User } from "../../models/user";
 import { UserService } from "../../services/user.service";
 import { UploadService } from "src/app/services/upload.service";
 import { PublicationService } from "src/app/services/publication.service";
+import * as $ from 'jquery';
 
 @Component({
     selector: 'timeline',
     templateUrl: './timeline.component.html',
-    providers:[UserService,UploadService,PublicationService]
+    providers: [UserService, UploadService, PublicationService]
 })
-export class TimelineComponent implements OnInit{
+export class TimelineComponent implements OnInit {
 
-    title:string
-    url:string
-    user:User
-    page:number
-    status:string
-    total:number
-    pages:number
-    msg:string
+    title: string
+    url: string
+    user: User
+    page: number
+    status: string
+    total: number
+    pages: number
+    msg: string
+    itemsPorPage: number
+    noMore:boolean;
     public stats: { follow: number, followme: number, publication: number };
-    public publications:any
-    
+    public publications: any
+
 
     constructor(
         private _route: ActivatedRoute,
         private _router: Router,
         private _userService: UserService,
         private _publicationService: PublicationService
-    ){
+    ) {
         this.title = "Timeline"
         this.url = GLOBAL.url
         this.user = this._userService.getDataUSer()
@@ -42,38 +45,44 @@ export class TimelineComponent implements OnInit{
         this.total = 0
         this.pages = 0
         this.msg = ''
+        this.itemsPorPage = 0
+        this.noMore = false;
     }
 
-    ngOnInit(){
+    ngOnInit() {
         console.log("componente timeline cargado correctamente")
         this.getPublications(this.page)
     }
 
-    getPublications(page=0){
+    getPublications(page = 0, adding = false) {
         this._publicationService.getPublications(page).subscribe(
             response => {
                 console.log(response)
-                if(!response.data){
+                if (!response.data) {
                     this.status = 'error'
-                }else{
+                } else {
 
                     this.status = response.status //estatus de la respuesta en este caso seria success
-                    this.total = response.totalRegistros //total de paginas del paginado entregado desde el api
+                    this.total = response.totalRegistros //total de registros entregado desde el api
                     this.pages = response.totalPaginas //total de paginas del paginado entregado desde el api
-                    this.publications = response.data//datos en un arreglo de objetos de todos los usuarios exceto el logueado
-                    
-                    if(page > this.pages){
-                        this._router.navigate(['/home'])
+                    this.itemsPorPage = response.numRegistrosMostrarXPagina // numero de registros entregados por pagina
+
+                    if (!adding) {
+                        this.publications = response.data//datos en un arreglo de objetos de todos los usuarios exceto el logueado
+                    } else {
+                        let arrayA = this.publications;
+                        let arrayB = response.data
+                        this.publications = arrayA.concat(arrayB);
+
+                        $("html,body").animate({
+                            scrollTop: $('body').prop('scrollHeight')
+                        })
                     }
-                    /*
-                    this.follows = response.follows//arreglo que tiene los id de los usuarios que ya sigo como usuario logueado
-                    this.totalPage = Array.from({ length: this.total }, (_, i) => i + 1); // Genera un array para la paginacion numerada 1,2,3,4
-                    */
                 }
             },
             error => {
                 let errorMessage = <any>error
-                if(errorMessage != null){
+                if (errorMessage != null) {
                     this.status = 'error'
                     this.msg = error.error.msg
                 }
@@ -81,5 +90,11 @@ export class TimelineComponent implements OnInit{
         )
     }
 
-
+    viewMore() {
+        this.page += 1;
+        if (this.page == this.pages) {
+            this.noMore = true;
+        }
+        this.getPublications(this.page, true);
+    }
 }
