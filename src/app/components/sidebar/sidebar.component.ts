cf822be4c1f6cdb3cf750465129f25e87d154ff8
Gustavo <versace123mx@gmail.com>
2024-08-07 14:02:19 -0600

    agregar funcionalidad de cargar imagen

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,13 +1,16 @@
 import { Component, EventEmitter, OnInit, Input, Output } from "@angular/core";
+import { Router, ActivatedRoute, Params } from "@angular/router";
 import { UserService } from "../../services/user.service";
 import { GLOBAL } from "../../services/global";
 import { User } from "../../models/user";
 import { Publication } from "../../models/publication";
 import { PublicationService } from "../../services/publication.service";
+import { UploadService } from "../../services/upload.service";
+
 @Component({
     selector: 'sidebar',
     templateUrl: './sidebar.component.html',
-    providers: [UserService, PublicationService]
+    providers: [UserService, PublicationService,UploadService]
 })
 export class SidebarComponent implements OnInit {
     public url: string
@@ -17,7 +20,12 @@ export class SidebarComponent implements OnInit {
     public publication: Publication
     public status: string
     public msg: string
-    constructor(private _userService: UserService, private _publicationService: PublicationService) {
+    public filesToUpload: Array<File>;
+
+    constructor(private _userService: UserService, 
+                private _publicationService: PublicationService,
+                private _uploadService: UploadService,
+                private _router: Router) {
         this.url = GLOBAL.url;
         this.user = this._userService.getDataUSer()
         this.stats = this._userService.getStats()
@@ -25,6 +33,7 @@ export class SidebarComponent implements OnInit {
         this.publication = new Publication('', '')
         this.status = ''
         this.msg = ''
+        this.filesToUpload = []
     }
 
     ngOnInit() {
@@ -43,14 +52,27 @@ export class SidebarComponent implements OnInit {
                 console.log(response)
                 this.msg = response.msg
 
+                //Obtenemos los datos del localstorage actual para obtener ul token
+                let datosUser = JSON.parse(localStorage.getItem('data') ?? '{}')
+
                 //Obtenemos los datos del localstorage actual
                 let datosPublication = JSON.parse(localStorage.getItem('stats') ?? '{}')
                 //realizamos la modificaciones pertinentes
                 datosPublication.publication = Number(datosPublication.publication)+1
                 localStorage.setItem('stats', JSON.stringify(datosPublication))
 
+                //Dice el victor que aqui tendria que ir la carga de la imagen
+                this._uploadService.makeFileRequest(this.url+'/publication/uploadfile/'+response.data.uid,this.filesToUpload,datosUser.token,'archivo')
+                .then((result:any) => {
+                    //datosUser.imagen = result.data
+                    //this.newImagen = result.data
+                    //volvemos a actualiza el localstorga
+                    //localStorage.setItem('data',JSON.stringify(datosUser))
 
-                form.reset();
+                    form.reset();
+                    this._router.navigate(['/timeline'])
+                })
+
             },
             error => {
                 this.status = 'error'
@@ -63,5 +85,11 @@ export class SidebarComponent implements OnInit {
     @Output() sended = new EventEmitter();
     sendPublication(){
         this.sended.emit()
+    }
+
+    fileChangeEvent(fileInput:any){
+        //console.log(fileInput.target.files[0])
+        this.filesToUpload = <Array<File>>fileInput.target.files;
+        //console.log(this.filesToUpload)
     }
 }
